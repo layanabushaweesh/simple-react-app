4b265e28db846c62d501181363ef7a226264bc0d
layanabushaweesh <layanabushaweesh@gmail.com>
2021-10-15 20:55:25 +0300

    export and import

---
+++ src/module.js
@@ -0,0 +1,16 @@
+export const x =2;
+export const y =2;
+
+export function Test() {
+   console.log("react js");
+}
+
+function Test2() {
+    console.log("defult react js")
+}
+
+export default Test2
+
+// just export i can repeat it many times in one page => when i use import i use {}
+// export defalut cant be changeed in one page just once => when i use import with out use {}
+// export defalut i can use impoet with another name and use it => it have the proierty => i can change the name
