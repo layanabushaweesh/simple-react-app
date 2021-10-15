0beee80f9747665e49c075cd3960ada59c72b2a8
layanabushaweesh <layanabushaweesh@gmail.com>
2021-10-15 21:08:35 +0300

    export and import

--- src/module.js
+++ src/module.js
@@ -12,5 +12,6 @@ function Test2() {
 export default Test2
 
 // just export i can repeat it many times in one page => when i use import i use {}
-// export defalut cant be changeed in one page just once => when i use import with out use {}
+// export defalut cant be repeted in one page just ONCE => when i use import without use {}
+// import for export just should have the same name
 // export defalut i can use impoet with another name and use it => it have the proierty => i can change the name
